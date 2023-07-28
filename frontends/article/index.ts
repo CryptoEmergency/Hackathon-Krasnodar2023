d0569c26cc7405e552d71be06b60a15dad2204a2
Betarost <betarost@yandex.ru>
2023-07-28 16:21:19 +0300

    Revert "Initial"
    This reverts commit 633a52c8a8cc4d5240f947e633a530ba4b3871b5.

--- frontends/article/index.ts
+++
@@ -1,9 +0,0 @@
-import { Micro } from "cemjs-all"
-import { loader } from "./loader"
-import { display } from "./display"
-
-export const micro: Micro = {
-    name: "MicroTest",
-    loader,
-    display
-}
