657a2f69723aa517d57767a37b4182ff6127246c
Maks <maksondmb2018@gmail.com>
2023-07-28 22:58:59 +0300

    fix admin

--- frontends/adminBank/display.tsx
+++ frontends/adminBank/display.tsx
@@ -5,14 +5,8 @@ export const display = function () {
         <div class="admin">
             <div class="admin__container">
                 <h2>Административная панель</h2>
-                <div class="admin__wrapper">
-                    <h4>Банки</h4>
-                </div>
-                <div class="admin__wrapper">
-                    <h4>Университеты</h4>
-                </div>
+                
             </div>
         </div>
     )
-
 }
--- frontends/adminRegistration/display.tsx
+++ frontends/adminRegistration/display.tsx
@@ -6,8 +6,18 @@ export const display = function () {
             <div class="admin_login">
             <form class="auto-form">
                 <h2 class="auth-title">
-                   Регистрация
+                    Регистрация
                 </h2>
+                <div class="auth__direction">
+                    <div class="auth__direction_wrapper">
+                        <label htmlFor="bank">Банк</label>
+                        <input id="bank" name="direction" type="radio" />
+                    </div>
+                    <div class="auth__direction_wrapper">
+                        <label htmlFor="university">Университет</label>
+                        <input id="university" name="direction" type="radio" />
+                    </div>
+                </div>
                 <div class="input_login" id="auth-label1">
                     <label class="auth-label" >E-mail</label>
                     <input class="auth-input" type="text" placeholder="Введите Ваш E-mail"></input>
@@ -15,13 +25,13 @@ export const display = function () {
                 <div  class="input_login" >
                     <label  class="auth-label" >Пароль</label>
                     <input class="auth-input" type="password" placeholder="Введите ваш пароль" 
-                   
+                
                     ></input>
                 </div>
                 <div  class="input_login" >
                     <label  class="auth-label" >Подтвердите пароль</label>
                     <input class="auth-input" type="password" placeholder="Введите ваш пароль(повторно)" 
-                   
+                
                     ></input>
                 </div>
                 <div class="btn_enter" id='auth-label2'>
