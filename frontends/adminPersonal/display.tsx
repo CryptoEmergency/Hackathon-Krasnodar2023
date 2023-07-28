e850309c919bdbd1cf1b85554ec94c29745b9785
Maks <maksondmb2018@gmail.com>
2023-07-28 21:50:50 +0300

    fix style

--- frontends/adminPersonal/display.tsx
+++ frontends/adminPersonal/display.tsx
@@ -1,12 +1,35 @@
 import { Cemjsx } from "cemjs-all"
 
+const bank=["Сбербанк", "Альфа-банк", "Совкомбанк"]
+const university=["Крипто университет", "МГУ", "БНТУ", "Технический университет"]
+
 export const display = function () {
     return (
         <div class="admin">
             <div class="admin__container">
                 <h2>Административная панель</h2>
-                <div class="admin__wrapper">
+                <div class="c__wrapper">
                     <h4>Банки</h4>
+                    <ul class="admin__bank_list">
+                        {
+                            bank.map((item, index) => {
+                                return (
+                                    <li class="admin-bank_item">
+                                        <span>{item}</span>
+                                        <input 
+                                            type="checkbox" 
+                                            ref={`${index}`}
+                                            checked={this.Ref[index]?.checked}
+                                            onclick={() => {
+                                                console.log(this.Ref[index])
+                                            }}
+                                        />
+                                        <label></label>
+                                    </li> 
+                                )
+                            })
+                        }
+                    </ul>
                 </div>
                 <div class="admin__wrapper">
                     <h4>Университеты</h4>
