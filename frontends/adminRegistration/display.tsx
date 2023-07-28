c22a0676bad554fd7443e7c54344483c8deae23f
Terriblefool <119065497+Terriblefool@users.noreply.github.com>
2023-07-28 22:20:32 +0300

    fix cemconfig

---
+++ frontends/adminRegistration/display.tsx
@@ -0,0 +1,31 @@
+import { Cemjsx } from "cemjs-all"
+
+export const display = function () {
+    return (
+        <main class="admin__inner">
+            <div class="admin_login">
+            <form class="auto-form">
+                <h2 class="auth-title">
+                   Авторизация 
+                </h2>
+                <div class="input_login" id="auth-label1">
+                    <label class="auth-label" >E-mail</label>
+                    <input class="auth-input" type="text" placeholder="Введите Ваш E-mail"></input>
+                </div>
+                <div  class="input_login" >
+                    <label  class="auth-label" >Пароль</label>
+                    <input class="auth-input" type="password" placeholder="Введите ваш пароль" 
+                   
+                    ></input>
+                </div>
+                <div class="btn_enter" id='auth-label2'>
+                    <button class="btn_enter1">
+                        Войти
+                    </button>
+                </div>
+            </form>
+            </div>
+        </main>
+    )
+
+}
