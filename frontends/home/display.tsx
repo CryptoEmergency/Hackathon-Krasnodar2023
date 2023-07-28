f3853ca8afd81fd21b79743c3ec6a26f36ce22a3
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-07-28 21:51:48 +0300

    section courses and univercity

--- frontends/home/display.tsx
+++ frontends/home/display.tsx
@@ -1,5 +1,14 @@
 import { Cemjsx } from "cemjs-all"
 import arrowBtn from '@svg/icons/arrowBtn.svg'
+import desing from '@svg/courses/designer.svg'
+import game from '@svg/courses/game.svg'
+import analyst from '@svg/courses/analyst.svg'
+import internetMarketer from '@svg/courses/internetMarketer.svg'
+import video from '@svg/courses/video.svg'
+import frontend from '@svg/courses/frontend.svg'
+import food from '@svg/courses/food.svg'
+import psychology from '@svg/courses/psychology.svg'
+
 
 const courses = [
     {
@@ -7,56 +16,64 @@ const courses = [
         price: '199 ₽',
         direction: 'Дизайн',
         lessons: 10,
-        tasks: 12
+        tasks: 12,
+        img: desing,
     },
     {
         title: 'Интернет-маркетолог',
         price: '215 ₽',
         direction: 'Фото и видео',
         lessons: 19,
-        tasks: 14
+        tasks: 14,
+        img: internetMarketer, 
     },
     {
         title: 'Видеосъмка и видеомонтаж',
         price: '115 ₽',
-        direction: 'Дизайн',
+        direction: 'Фото и видео',
         lessons: 10,
-        tasks: 12
+        tasks: 12, 
+        img: video,
     },
     {
         title: 'Frontend-разработчик',
         price: '119 ₽',
         direction: 'Программирование',
         lessons: 20,
-        tasks: 30
+        tasks: 30,
+        img: frontend,
     },
     {
         title: 'Бизнес-аналитик',
         price: '119 ₽',
         direction: 'Управление',
         lessons: 3,
-        tasks: 9
+        tasks: 9,
+        img: analyst,
     },
     {
         title: 'Правильное питание',
         price: '216 ₽',
         direction: 'Питание',
         lessons: 5,
-        tasks: 7
+        tasks: 7,
+        img: food,
     },
     {
         title: 'Разработчик игр на Unreal Engine',
         price: '99 ₽',
         direction: 'Игры',
         lessons: 2,
-        tasks: 5
+        tasks: 5,
+        img: game,
     },
     {
         title: 'Детская психология',
         price: '216 ₽',
         direction: 'Психология',
         lessons: 2,
-        tasks: 5
+        tasks: 5,
+        img: psychology
     },
 ]
 
@@ -86,9 +103,10 @@ export const display = function () {
                                             <span class="course_price">{item.price}</span>
                                             <ul class="course_desc">
                                                 <li>{item.direction}</li>
-                                                <li>{item.lessons}</li>
-                                                <li>{item.tasks}</li>
+                                                <li>{`${item.lessons} уроков`}</li>
+                                                <li>{`${item.tasks} заданий`}</li>
                                             </ul>
+                                            <img></img>
                                         </div>
                                     )
                                 })
