1c676b7f47535318eec3520f58c8d54567caf3be
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-07-28 19:53:56 +0300

    .

--- frontends/header/display.tsx
+++ frontends/header/display.tsx
@@ -79,7 +79,7 @@ export const display = function () {
                                 }}>
                                 <img src={search}></img>
                             </div>
-                            <div class="header_education">
+                            {/* <div class="header_education">
                                 <span 
                                     class={["header_education_item", 
                                     this.Static.education == 'СПО' ? "header_education_item_active" : null]}
@@ -100,52 +100,57 @@ export const display = function () {
                                 >
                                     Высшее
                                 </span>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
-                <div class={["header_search_box", this.Static.search ? "header_search_box_active" : null]}>
-                    <div class="search_box">
-                        <div 
-                            class="dropdown"
-                            onclick={()=>{
-                                this.Static.dropdown = !this.Static.dropdown;
-                                this.init();
-                            }} 
-                        >
-                            <div class="dropdown_header">{this.Static.dropdownHeader}</div>
-                            <ul 
-                                class={["dropdown_list", 
-                                this.Static.dropdown ? "dropdown_list_open" : null]}
+                
+            </div>
+            <div class={["header_search_box", this.Static.search ? "header_search_box_active" : null]}>
+                <div class="wrapper">
+                    <div class="header_search_box_inner">
+                        <div class="search_box">
+                            {/* <div 
+                                class="dropdown"
+                                onclick={()=>{
+                                    this.Static.dropdown = !this.Static.dropdown;
+                                    this.init();
+                                }} 
                             >
-                                {
-                                    universe.map((item, index)=>{
-                                        return(
-                                            <li class="dropdown_list_item">{item.name}</li>
-                                        )
-                                    })
-                                }
-                            </ul>
+                                <div class="dropdown_header">{this.Static.dropdownHeader}</div>
+                                <ul 
+                                    class={["dropdown_list", 
+                                    this.Static.dropdown ? "dropdown_list_open" : null]}
+                                >
+                                    {
+                                        universe.map((item, index)=>{
+                                            return(
+                                                <li class="dropdown_list_item">{item.name}</li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div> */}
+                            <input 
+                                type="search"
+                                placeholder="Что Вас интересует ?" 
+                                class="header_input_search"
+                            ></input>
+                            <button class="btn search_blue">
+                                <img src={searchWhite}></img>
+                            </button>
                         </div>
-                        <input 
-                            type="search"
-                            placeholder="Что Вас интересует ?" 
-                            class="header_input_search"
-                        ></input>
-                        <button class="btn search_blue">
-                            <img src={searchWhite}></img>
+                        <button 
+                            class="btn btn_close"
+                            onclick={()=>{
+                                this.Static.search = !this.Static.search;
+                                this.init();
+                            }}
+                        >
+                            {/* <img src={close}></img> */}
+                            X
                         </button>
                     </div>
-                    <button 
-                        class="btn btn_close"
-                        onclick={()=>{
-                            this.Static.search = !this.Static.search;
-                            this.init();
-                        }}
-                    >
-                        {/* <img src={close}></img> */}
-                        X
-                    </button>
                 </div>
             </div>
         </header>
