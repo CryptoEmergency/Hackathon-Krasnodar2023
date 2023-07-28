7016f39590eab48a27dfe01680e05c690f65b865
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-07-28 21:29:05 +0300

    .

--- frontends/home/display.tsx
+++ frontends/home/display.tsx
@@ -1,4 +1,5 @@
 import { Cemjsx } from "cemjs-all"
+import arrowBtn from '@svg/icons/arrowBtn.svg'
 
 const courses = [
     {
@@ -66,7 +67,7 @@ export const display = function () {
                 <div class="home_inner">
                     {/* <section class="universities">
                         <h2>Вузы</h2>
-                        <div class="universities-wrap">
+                        <div class="universities_wrap">
                             <button class="universities_prev"></button>
                             <button class="universities_next"></button>
                             <div class="universities_carousel">
@@ -75,7 +76,30 @@ export const display = function () {
                         </div>
                     </section>  */}
                     <section class="courses">
-
+                        <h2>Курсы</h2>
+                        <div class="courses_wrap">
+                            {
+                                courses.map((item, index)=>{
+                                    return(
+                                        <div class={["course", `course_${index}`]}>
+                                            <h5 class="course_title">{item.title}</h5>
+                                            <span class="course_price">{item.price}</span>
+                                            <ul class="course_desc">
+                                                <li>{item.direction}</li>
+                                                <li>{item.lessons}</li>
+                                                <li>{item.tasks}</li>
+                                            </ul>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <div class="f-center">
+                                <button class="btn btn_default">
+                                    <span>Посмотреть все курсы</span>
+                                    <img src={arrowBtn}></img>
+                                </button>
+                            </div>
+                        </div>
                     </section>
                 </div>
             </div>
