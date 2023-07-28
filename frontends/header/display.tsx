b1f0d9d46a70d8ab22e41924432b0e06d2339e34
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-07-28 17:24:44 +0300

    Processing header

--- frontends/header/display.tsx
+++ frontends/header/display.tsx
@@ -1,14 +1,18 @@
 import { Cemjsx } from "cemjs-all"
 import logo from '@svg/logo.svg'
+import search from '@svg/icons/search.svg'
+import close from '@svg/icons/close.svg'
 
 export const display = function () {
     return (
         <header class="header">
             <div class="wrapper">
                 <div class="header_inner">
+
                     <a href="/" onclick={this.Fn.link} class="header_logo">
                         <img src={logo}></img>
                     </a>
+                    
                     <div class="header_menu">
                         <div
                             class="menu_icon"
@@ -25,52 +29,69 @@ export const display = function () {
                         </div>
                         <nav>
                             <ul class="header_menu_list">
-                                <li class="header_menu_list_item">Вузы</li>
-                                <li class="header_menu_list_item">Банки</li>
-                                <li class="header_menu_list_item">Программы</li>
-                                <li class="header_menu_list_item">Тесты</li>
-                                <li class="header_menu_list_item">Абиетурентам</li>
-                                <li class="header_menu_list_item">Статьи</li>
+                                <li class="header_menu_list_item">
+                                    <a href="#">Вузы</a>
+                                </li>
+                                <li class="header_menu_list_item">
+                                    <a href="#">Банки</a>
+                                </li>
+                                <li class="header_menu_list_item">
+                                    <a href="#">Программы</a>
+                                </li>
+                                <li class="header_menu_list_item">
+                                    <a href="#">Тесты</a>
+                                </li>
+                                <li class="header_menu_list_item">
+                                    <a href="#">Абиетурентам</a>
+                                </li>
+                                <li class="header_menu_list_item">
+                                    <a href="#">Статьи</a>
+                                </li>
                             </ul>
                         </nav>
+                        <div class="header_details">
+                            <div class="header_icon">
+                                <img src={search}></img>
+                            </div>
+                            <div class="header_education">
+                                <span 
+                                    class={["header_education_item", 
+                                    this.Static.education == 'СПО' ? "header_education_item_active" : null]}
+                                    onclick={()=>{
+                                        this.Static.education = 'СПО';
+                                        this.init();
+                                    }}
+                                >
+                                    СПО
+                                </span>
+                                <span 
+                                    class={["header_education_item", 
+                                    this.Static.education == 'Высшее' ? "header_education_item_active" : null]}
+                                    onclick={()=>{
+                                        this.Static.education = 'Высшее';
+                                        this.init();
+                                    }}
+                                >
+                                    Высшее
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-
+                <div class="header_search_box">
+                    <div class="search_box">
+                        <div class="dropdown">
+                            <span>Выбрать вуз</span>
+                            
+                        </div>
+                        <input type="search">Что Вас интересует ?</input>
+                    </div>
+                    <button class="btn btn_close">
+                        <img src={close}></img>
+                    </button>
+                </div>
             </div>
         </header>
-        // <header class="header header_container">
-        //     <div class="header_inner">
-        //         <div class="header_logo">
-        //             <a href="/" onclick={this.Fn.link}>
-        //                 <img
-        //                     class="header_logo-img"
-        //                     src={logo}
-        //                 ></img>
-        //             </a>
-        //         </div>
-        //         <nav>
-        //             <ul class="header_menu">
-        //                 <li
-        //                     class={["header_menu_item", this.Static.page == "cemjs" ? "header_menu_item-active" : null]}
-
-        //                 ><a href="/about/" onclick={this.Fn.link}>Cem JS</a>
-        //                 </li>
-        //                 <li
-        //                     class={["header_menu_item", this.Static.page == "examples" ? "header_menu_item-active" : null]}
-        //                 ><a href="https://ya.ru" onclick={this.Fn.link}>Examples</a></li>
-        //                 <li
-        //                     class={["header_menu_item", this.Static.page == "contacts" ? "header_menu_item-active" : null]}
-        //                     onclick={() => {
-        //                         this.Static.page = "contacts"
-        //                         this.init()
-        //                     }}
-        //                 >Contacts</li>
-        //             </ul>
-        //         </nav>
-        //     </div>
-        // </header>
     )
 
 }
