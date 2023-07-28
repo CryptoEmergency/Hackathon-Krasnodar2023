51eaea0e719d3a0717d95d055d41a851a5862eb6
Terriblefool <119065497+Terriblefool@users.noreply.github.com>
2023-07-29 01:53:04 +0300

    admin univer

--- frontends/adminUniversity/display.tsx
+++ frontends/adminUniversity/display.tsx
@@ -13,15 +13,26 @@ export const display = function () {
                         <label  class="auth-label" >Город</label>
                         <input class="auth-input" type="text" placeholder="Введите назвнание города"></input>
                     </div>
-                    <div  class="input_login" id="auth-label1">
+                    <div  class="input_login" id="auth-label2">
                         <label  class="auth-label" >Название факультета</label>
                         <input class="auth-input" type="text" placeholder="Введите назвнание факультета"></input>
                     </div>
-                    <div class="btn_enter_2" id='auth-label2'>
-                    <button class="btn_enter1">
+                    <div class="btn_enter_3" id='auth-label2'>
+                    <button class="btn_enter1"
+                    
+                    onclick={()=>{
+                        this.Static.faculty.push({})
+                        this.init()
+                    }}
+
+                    
+                    >
                         Внести данные
                     </button>
-                </div>   
+                    {
+                        this.Static.faculty.map((index,item)=>{
+                            return(
+                                <div>
                     <div  class="input_login" id="auth-label1">
                         <label  class="auth-label" >Специализация</label>
                         <input class="auth-input" type="text" placeholder="Введите назвнание специализации"></input>
@@ -42,6 +53,30 @@ export const display = function () {
                         <label  class="auth-label" >Стоимость</label>
                         <input class="auth-input" type="number" placeholder="Введите стоимость"></input>
                     </div>
+                    </div>
+                         ) })
+                    }
+                </div>   
+                    {/* <div  class="input_login" id="auth-label1">
+                        <label  class="auth-label" >Специализация</label>
+                        <input class="auth-input" type="text" placeholder="Введите назвнание специализации"></input>
+                    </div>
+                    <div  class="input_login" id="auth-label1">
+                        <label  class="auth-label" >Количество мест</label>
+                        <input class="auth-input" type="number" placeholder="Введите количество мест"></input>
+                    </div>
+                    <div  class="input_login" id="auth-label1">
+                        <label  class="auth-label" >Форма обучения</label>
+                        <input class="auth-input" type="text" placeholder="Введите форму обучния"></input>
+                    </div>
+                    <div  class="input_login" id="auth-label1">
+                        <label  class="auth-label" >Время обучения</label>
+                        <input class="auth-input" type="number" placeholder="Введите время обучения"></input>
+                    </div>
+                    <div  class="input_login" id="auth-label1">
+                        <label  class="auth-label" >Стоимость</label>
+                        <input class="auth-input" type="number" placeholder="Введите стоимость"></input>
+                    </div> */}
 
             </div>
         </div>
