275fdf090dfc7ee176c0d1e0ff59e84e0194b101
Maks <maksondmb2018@gmail.com>
2023-07-29 19:56:51 +0300

    fix form

--- frontends/formResponse/display.tsx
+++ frontends/formResponse/display.tsx
@@ -1,16 +1,16 @@
 import { Cemjsx } from "cemjs-all"
 import sber from '@images/sberbank.png'
 import alfa from '@svg/banks/alfa1.png'
-import rnkb from '@svg/banks/rnkb.svg'
+import raiffeisen from '@svg/banks/raiff2.png'
+import agree from '@svg/agree.svg'
+import rejection from '@svg/rejection.svg'
 
 export const display = function () {
-    console.log(this.Static.time)
     return (
         <div class="main__modal">
             <div class="modal__conteiner">
                 <div class="title__st">
                     <span>Ожидание ответа</span>
-                    <div class="spinner"></div>
                 </div>
                 
                 <div class="horizontal-line">
@@ -20,16 +20,45 @@ export const display = function () {
                 <div class="modal__bank">
                     <div class="modal__bank_item">
                         <span>Сбербанк</span>
-                        <img src={sber} />
+                        <img class="modal__bank_logo" src={sber} />
                     </div>
-                    <div class="modal__bank">
-                        <span>Ожидаем ответа</span>
-                        <div class="spinner"></div>
-                    </div>
+                    {
+                        this.Static.sber.active
+                        ?
+                        <img class="modal__bank_img" src={rejection} />
+                        :
+                        <div class="spinner spinner_pending"></div>
+                    }
                 </div>
 
-                
+                <div class="modal__bank">
+                    <div class="modal__bank_item">
+                        <span>Альфа-Банк</span>
+                        <img class="modal__bank_logo" src={alfa} />
+                    </div>
+                    {
+                        this.Static.alfa.active
+                        ?
+                        <img class="modal__bank_img" src={agree} />
+                        :
+                        <div class="spinner spinner_pending"></div>
+                    }
+                    
+                </div>
 
+                <div class="modal__bank">
+                    <div class="modal__bank_item">
+                        <span>Райффайзенбанк</span>
+                        <img class="modal__bank_logo" src={raiffeisen} />
+                    </div>
+                    {
+                        this.Static.raiffeisen.active
+                        ?
+                        <img class="modal__bank_img" src={rejection} />
+                        :
+                        <div class="spinner spinner_pending"></div>
+                    }
+                </div>
             </div>
         </div>
     )
--- frontends/formResponse/loader.ts
+++ frontends/formResponse/loader.ts
@@ -1,14 +1,31 @@
 export const loader = function () {
-    this.Static.time = {
-        one: 1000,
-        two: 2000,
-        three: 3000,
-        four: 4000,
-        five: 5000,
-        six: 6000
+    this.Static.sber = {
+        active: false,
+        time: 3000,
     }
 
-    this.Static.timer = setInterval(() => {
-        
-    }, 2000);
+    this.Static.alfa = {
+        active: false,
+        time: 5000,
+    }
+
+    this.Static.raiffeisen = {
+        active: false,
+        time: 6000,
+    }
+
+    setTimeout(() => {
+        this.Static.sber.active = true
+        this.init()
+    }, this.Static.sber.time);
+
+    setTimeout(() => {
+        this.Static.alfa.active = true
+        this.init()
+    }, this.Static.alfa.time);
+
+    setTimeout(() => {
+        this.Static.raiffeisen.active = true
+        this.init()
+    }, this.Static.raiffeisen.time);
 }
