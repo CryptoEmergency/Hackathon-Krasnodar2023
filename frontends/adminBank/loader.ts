6ac640781234366d50028d0ee8a4b1514aa15efd
Betarost <betarost@yandex.ru>
2023-07-29 23:41:46 +0300

    .

--- frontends/adminBank/loader.ts
+++ frontends/adminBank/loader.ts
@@ -1,9 +1,23 @@
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
 export const loader = function () {
     this.Static.bank = {
         name: "",
         credit: []
     }
-
+    // const iv = crypto.getRandomValues(new Uint8Array(16));
+    console.log(makeid(16))
     // fetch("/api/user/bank/set", {
     //     method: "POST",
     //     headers: {
