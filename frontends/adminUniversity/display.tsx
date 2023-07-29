f5c4fde9df7b250c70bb6d951689fb80221259e8
Terriblefool <119065497+Terriblefool@users.noreply.github.com>
2023-07-29 05:35:57 +0300

    fix university

--- frontends/adminUniversity/display.tsx
+++ frontends/adminUniversity/display.tsx
@@ -1,10 +1,10 @@
 import { Cemjsx } from "cemjs-all"
-
+import iconDelete from "@svg/delete_icon.svg"
 export const display = function () {
     return (
-        <div class="admin">
+        <div class="admin__univer">
             <div class="admin__container">
-                <h2>Административная панель</h2>
+                <h2 style="margin-bottom:15px">Административная панель</h2>
                 <div  class="input_login" id="auth-label1">
                         <label  class="auth-label" >Название университета</label>
                         <input class="auth-input" type="text" placeholder="Введите назвнание университета"></input>
@@ -18,7 +18,7 @@ export const display = function () {
                         <input class="auth-input" type="text" placeholder="Введите назвнание факультета"></input>
                     </div>
                     <div class="btn_enter_3" id='auth-label2'>
-                    <button class="btn_enter1"
+                    <button class="btn_enter1" id="btn-enter2"
                     
                     onclick={()=>{
                         this.Static.faculty.push({})
@@ -30,31 +30,39 @@ export const display = function () {
                         Внести данные
                     </button>
                     {
-                        this.Static.faculty.map((index,item)=>{
+                        this.Static.faculty.map((item,index)=>{
                             return(
-                                <div>
-                    <div  class="input_login" id="auth-label1">
-                        <label  class="auth-label" >Специализация</label>
-                        <input class="auth-input" type="text" placeholder="Введите назвнание специализации"></input>
-                    </div>
-                    <div  class="input_login" id="auth-label1">
-                        <label  class="auth-label" >Количество мест</label>
-                        <input class="auth-input" type="number" placeholder="Введите количество мест"></input>
-                    </div>
-                    <div  class="input_login" id="auth-label1">
-                        <label  class="auth-label" >Форма обучения</label>
-                        <input class="auth-input" type="text" placeholder="Введите форму обучния"></input>
-                    </div>
-                    <div  class="input_login" id="auth-label1">
-                        <label  class="auth-label" >Время обучения</label>
-                        <input class="auth-input" type="number" placeholder="Введите время обучения"></input>
-                    </div>
-                    <div  class="input_login" id="auth-label1">
-                        <label  class="auth-label" >Стоимость</label>
-                        <input class="auth-input" type="number" placeholder="Введите стоимость"></input>
-                    </div>
-                    </div>
-                         ) })
+                                <div class="speciality__conteiner">
+                                    <img class="delete_icon" src={iconDelete}
+                                        onclick={()=>{
+                                            this.Static.faculty.splice(index, 1)
+                                            this.init()
+                                        }}
+                                        >                        
+                                    </img>    
+                                    <div  class="input_login" id="auth-label1">                                
+                                        <label  class="auth-label" >Специализация</label>
+                                        <input class="auth-input" type="text" placeholder="Введите назвнание специализации"></input>
+                                    </div>
+                                    <div  class="input_login" id="auth-label1">
+                                        <label  class="auth-label" >Количество мест</label>
+                                        <input class="auth-input" type="number" placeholder="Введите количество мест"></input>
+                                    </div>
+                                    <div  class="input_login" id="auth-label1">
+                                        <label  class="auth-label" >Форма обучения</label>
+                                        <input class="auth-input" type="text" placeholder="Введите форму обучния"></input>
+                                    </div>
+                                    <div  class="input_login" id="auth-label1">
+                                        <label  class="auth-label" >Время обучения</label>
+                                        <input class="auth-input" type="number" placeholder="Введите время обучения"></input>
+                                    </div>
+                                    <div  class="input_login" id="auth-label4">
+                                        <label  class="auth-label" >Стоимость</label>
+                                        <input class="auth-input" type="number" placeholder="Введите стоимость"></input>
+                                    </div>
+                                </div>
+                             ) 
+                        })
                     }
                 </div>   
                     {/* <div  class="input_login" id="auth-label1">
