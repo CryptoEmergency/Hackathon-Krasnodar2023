a5f5e2aff5c0217ccc9ef3c339ff3281f31b62bf
Betarost <betarost@yandex.ru>
2023-07-29 11:02:37 +0300

    .

---
+++ frontends/banks/display copy.tsx
@@ -0,0 +1,139 @@
+import { Cemjsx } from "cemjs-all"
+import arrowBtn from '@svg/icons/arrowBtn.svg'
+import university1 from '@images/universities/1.jpg'
+import university2 from '@images/universities/2.jpg'
+import university3 from '@images/universities/3.jpg'
+import sber from '@svg/banks/sber.svg'
+import alfa from '@svg/banks/alfa.png'
+import rnkb from '@svg/banks/rnkb.svg'
+import vtb from '@svg/banks/vtb.png'
+import raiffeisen from '@svg/banks/raiff.png'
+
+// const universitiesCards =[
+//     {
+//         id: 1,
+//         title: "Санкт-Петербургский реставрационно-строительный институт",
+//         city: "Санкт-Петербург",
+//         price_from: "100 000",
+//         programms_number: "16",
+//         desc: "Дизайн; реставрация и ещё 9 направлений",
+//         picture: university1,
+//         bank: "ЦЕНТР-ИНВЕСТ"
+//     },
+//     {
+//         id: 2,
+//         title: "Санкт-Петербургский филиал Национального исследовательского университета «Высшая школа экономики»",
+//         city: "Санкт-Петербург",
+//         price_from: "360 000",
+//         programms_number: "43",
+//         desc: "Дизайн; реставрация и ещё 9 направлений",
+//         picture: university2,
+//         bank: "Тинькоф"
+//     },
+//     {
+//         id: 3,
+//         title: "Московский технический университет связи и информатики",
+//         city: "Санкт-Петербург",
+//         price_from: "80 000",
+//         programms_number: "16",
+//         desc: "Дизайн; реставрация и ещё 9 направлений",
+//         picture: university3,
+//         bank: "Райффайзен"
+//     },
+//     {
+//         id: 1,
+//         title: "Санкт-Петербургский реставрационно-строительный институт",
+//         city: "Санкт-Петербург",
+//         price_from: "100 000",
+//         programms_number: "16",
+//         desc: "Дизайн; реставрация и ещё 9 направлений",
+//         picture: university1,
+//         bank: "ЦЕНТР-ИНВЕСТ"
+//     },
+//     {
+//         id: 2,
+//         title: "Санкт-Петербургский филиал Национального исследовательского университета «Высшая школа экономики»",
+//         city: "Санкт-Петербург",
+//         price_from: "360 000",
+//         programms_number: "43",
+//         desc: "Дизайн; реставрация и ещё 9 направлений",
+//         picture: university2,
+//         bank: "Сбербанк"
+//     },
+//     {
+//         id: 3,
+//         title: "Московский технический университет связи и информатики",
+//         city: "Санкт-Петербург",
+//         price_from: "80 000",
+//         programms_number: "16",
+//         desc: "Дизайн; реставрация и ещё 9 направлений",
+//         picture: university3,
+//         bank: "Альфа Банк"
+//     },
+
+
+// ]
+
+const banks_logo=[
+    {
+        'sber' : sber
+    },
+    {
+        'alfa' :alfa
+    },
+    {
+        'rnkb' :  rnkb
+    },
+    {
+        'vtb' :vtb
+    },
+    {
+        'raiffeisen' : raiffeisen
+    },
+]
+
+export const display = function () {
+    console.log(this.records)
+    return (
+        <main class="home page">
+            <div class="wrapper">
+                <h2 class="section_banks">Банки</h2>
+                <section class="banks">
+                    <div class="banks_list">
+                        <div class="logo">
+                           
+                        </div>
+                        {
+                            this.records
+                                ?
+                                this.records.map((item, index) => {
+                                    // console.log(index)
+                                    return (
+                                        
+                                        <div class="bank_item">
+                
+                                            <a href=""><img src={sber}></img></a>
+                                    <div class="item_about">
+                                        <a>{item.credit[0].nameCredit}</a>
+                                        <a>{item.name}</a>
+                                        <div class= "price-offer">
+                                            {/* <p>{item.price_from}</p> */}
+                                            {/* <p><span>{item.bank}</span></p> */}
+                                        </div>
+                                        </div>
+
+                                    </div>
+                                    )
+                                })
+
+                                :
+                                null
+                        }
+
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
+
+}
