7874c146c142c92da14979e20a20cfff62b5c47e
Betarost <betarost@yandex.ru>
2023-07-29 04:26:01 +0300

    .

--- api/routers/lists.js
+++ api/routers/lists.js
@@ -1,21 +1,73 @@
-
-import OTest2 from './open/Test2.js'
-import OInstitute from './open/Institute.js'
-import Ovuz from './open/vuz.js'
-import BTest2 from './bank/Test2.js'
-import Bank from "./bank/Bank.js"
-import ATest2 from './academy/Test2.js'
-import UOrg from './user/Org.js'
-import Uvuz from './user/vuz.js'
+import { Api } from '../mongo.js';
 
 
-export default [
-    OTest2,
-    BTest2,
-    UOrg,
-    ATest2,
-    OInstitute,
-    Bank,
-    Uvuz,
-    Ovuz
+const OpenApi = [
+    {
+        method: "post",
+        url: "/open/Institute",
+        fn: async (req, res) => {
+            // let tmp = await Api.getInstitute({})
+            return res.json(await Api.getInstitute({}));
+        }
+    },
+    {
+        method: "post",
+        url: "/open/vuz",
+        fn: async (req, res) => {
+            return res.json(await Api.getVuz({}));
+
+
+        }
+    }
 ]
+
+const UserApi = [
+    {
+        method: "post",
+        url: "/user/Org/:type",
+        fn: async (req, res) => {
+            console.log("req.params", req.params)
+            console.log("request.body", req.body);
+            return res.json(await Api.getBank({}));
+        }
+    },
+    {
+        method: "post",
+        url: "/user/vuz/:type",
+        fn: async (req, res) => {
+            if (req.params.type == "get") {
+                return res.json(await Api.getVuz({}));
+
+            } else if (req.params.type == "set") {
+                return res.json(await Api.setVuz(req.body));
+            }
+
+
+        }
+    }
+]
+
+const BankApi = [
+    {
+        method: "post",
+        url: "/open/Bank/:type",
+        fn: async (req, res) => {
+            console.log(req.params.type)
+            if (req.params?.type == "set") {
+                console.log("request.body", req.body);
+                return res.json(await Api.setBank(req.body));
+            } else {
+                console.log("request.body get", req.params);
+                return res.json(await Api.getBank({}));
+            }
+            // console.log("req.params", req.params)
+
+        }
+    }
+]
+
+const VuzApi = [
+
+]
+
+export default [...OpenApi, ...UserApi, ...BankApi, ...VuzApi]
