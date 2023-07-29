ecd0b6a35301882ef0a955ed4c4f92aca170e868
Betarost <betarost@yandex.ru>
2023-07-29 15:22:20 +0300

    .

--- frontends/adminBank/loader.ts
+++ frontends/adminBank/loader.ts
@@ -4,15 +4,15 @@ export const loader = function () {
         credit: []
     }
 
-    fetch("/api/user/bank/set", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ filter: {} }),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-        });
+    // fetch("/api/user/bank/set", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ filter: {} }),
+    // })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //     });
 }
